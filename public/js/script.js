6b224ebc4506e2428f357032fd10f3b261a46628
ivanobelchenko <vanyaobelka@mail.ru>
2022-04-25 10:45:18 +0300

    two

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,6 @@
 let navHTML = document.getElementById('nav');
+let login = document.getElementById('login');
+let register = document.getElementById('register');
 
 if (localStorage.getItem('username') && localStorage.getItem('user_id')) {
   navHTML.insertAdjacentHTML('beforeend', `
@@ -6,12 +8,16 @@ if (localStorage.getItem('username') && localStorage.getItem('user_id')) {
     <p><a href="/profile">Профиль</a></p>
     <p><a id="logout">Выйти</a></p>
   `);
+  login.remove();
+  register.remove();
   
   let logout = document.getElementById('logout');
   
   logout.addEventListener('click', _ => {
     localStorage.removeItem('username');
     localStorage.removeItem('user_id');
+    navHTML.appendChild(login);
+    navHTML.appendChild(register);
     window.location.reload();
   });
 }
